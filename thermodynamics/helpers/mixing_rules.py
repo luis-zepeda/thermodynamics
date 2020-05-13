0b4408dbdac7753f981991cde973dacb873db5a7
luis-zepeda <luiszepedavarela@comunidad.unam.mx>
2020-05-13 01:27:04 -0500

    half way to van der waals mixing rule

--- thermodynamics/helpers/mixing_rules.py
+++ thermodynamics/helpers/mixing_rules.py
@@ -1,14 +1,12 @@
 from np import array,sum, sqrt
 
-def van_der_waals(compositions,binary_interactions,a,b):
-    if(len(compositions) != len(binary_interactions) or len(a) != len(b)):
-        return 'Compositions and binary interactions must be same dimensional'
-    
-    compositions= array(compositions)
-    binary_interactions = array(binary_interactions)
-    a=array(a)
-    b=array(b)
-    #Yt = (1.0/(M*N)) * sum([Y[i][j] for i in range(M) for j in range(N)])
-    b = sum(compositions*b)
-    a = sum(compositions[i]*compositions[j]*sqrt(a[i]*a[j])*(1-binary_interactions[i]) for i in range(len(compositions)) for j in range(len(compositions)))
-    return (a,b)
+def selector(mixing_rule):
+    if(mixing_rule == 'van_der_waals'):
+        return van_der_waals
+    else:
+        return 'Mixing rule does not exist'
+
+def van_der_waals(compositions,kij,Ai,Bi):
+    B = sum(compositions*Bi)
+    Aij=[sqrt(A[i]*A[j])*(1-kij[i][j]) for i in range(0,len(A)) for j in range(0,len(A))]
+    A = [sum(compositions[i]*compositions[j]*A[i+j])]
--- thermodynamics/mix/general/equilibrium.py
+++ thermodynamics/mix/general/equilibrium.py
@@ -4,22 +4,30 @@ from ..helpers.eosHelpers import A_fun, B_fun, getCubicCoefficients, dAdT_fun
 from ..solvers.cubicSolver import cubic_solver
 from ..helpers import temperatureCorrelations as tempCorr
 
-from numpy import log, exp, sqrt,absolute, array
+from numpy import log, exp, sqrt,absolute, array,sum
 from scipy.optimize import fsolve, newton, root
 from scipy.integrate import quad
 
 
-def solve_eos(t,p,tc,pc,acentric,compositions,method='pr',alfa_function='alfa_peng_robinson',diagram=False,properties=False,heat_capacity=None):
+def solve_eos(t,p,tc,pc,acentric,compositions,kij,method='pr',alfa_function='alfa_peng_robinson',mixing_rule='van_der_waals',diagram=False,properties=False,heat_capacity=None):
     # Vectorization
     tc = array(tc)
     pc= array(pc)
+    acentric = array(acentric)
     compositions=array(compositions)
-    
+    kij = array(kij)
     
     # Method selection
-    u,w,omega_a,omega_b,L = eos.selector(method)
+    eos_fun() = eos.selector(method)
+    u,w,omega_a,omega_b,L = eos_fun()
 
     # Alfa function selection    
     alfa_fun = alfaFunctions.selector(alfa_function)
     alfa= alfa_fun(t,tc,acentric)
-    
+
+    Ai = A_fun(t,p,tc,pc,acentric,omega_a,alfa)
+    Bi = B_fun(t,p,tc,pc,omega_b)
+
+    # esta es regla de mezclado 
+    B = sum(compositions*Bi)
+
